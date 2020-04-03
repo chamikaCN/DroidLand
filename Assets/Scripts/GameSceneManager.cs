70e6fd80f36f4f2d02176073e9495a2c605777da
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-03 14:42:54 +0530

    droid change available

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameSceneManager instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("many gamemanagers");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+    String playerTeam = "Orange", enemyTeam = "Blue";
+    Droid[] droids;
+    List<Droid> playerDroids, enemyDroids;
+    Droid currentDroid;
+    void Start()
+    {
+        playerDroids = new List<Droid>();
+        enemyDroids = new List<Droid>();
+        droids = GameObject.FindObjectsOfType<Droid>();
+        foreach (Droid droid in droids)
+        {
+            if (droid.tag.ToString() == playerTeam)
+            {
+                playerDroids.Add(droid);
+            }
+            else
+            {
+                enemyDroids.Add(droid);
+            }
+        }
+        int rand = UnityEngine.Random.Range(0, playerDroids.Count);
+        currentDroid = playerDroids[rand];
+        PlayerManager.instance.setCurrentDroid(currentDroid);
+        currentDroid.setPlayerControl();
+        CameraController.instance.setTransform(currentDroid.transform);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ChangeDroid();
+        }
+
+    }
+
+    void ChangeDroid()
+    {
+        currentDroid.ResetPlayerControl();
+        PlayerManager.instance.removeCurrentDroid();
+        int rand = UnityEngine.Random.Range(0, playerDroids.Count);
+        currentDroid = playerDroids[rand];
+        PlayerManager.instance.setCurrentDroid(currentDroid);
+        currentDroid.setPlayerControl();
+        CameraController.instance.setTransform(currentDroid.transform);
+    }
+
+    public Droid getCurrentDroid()
+    {
+        return currentDroid;
+    }
+}
