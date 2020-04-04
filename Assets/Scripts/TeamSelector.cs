340981725ca19b78d94f3a05c58c37cf1ce019f0
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-04 22:02:59 +0530

    team selection

---
+++ Assets/Scripts/TeamSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeamSelector : MonoBehaviour
+{
+    public void LoadGame(string team)
+    {
+        PlayerPrefs.SetString("Team", team);
+        SceneManager.LoadScene("DevScene");
+    }
+}
