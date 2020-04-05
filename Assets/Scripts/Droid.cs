92f83514c1996514a6b5e9083d0a128349628be9
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-05 17:46:42 +0530

    attack and range check on dev

--- Assets/Scripts/Droid.cs
+++ Assets/Scripts/Droid.cs
@@ -8,24 +8,43 @@ public class Droid : MonoBehaviour
     NavMeshAgent agent;
     GameObject[] goals;
     GameObject currentGoal;
-    bool playerControlled;
+    public GameObject ball;
+    bool playerControlled, attacked;
+    int enemiesInRange;
+    List<Transform> detectedEnemies;
+    string Team;
+    Vector3 movingDirection;
+    Transform currentEnemyTransform;
 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
 
+        Team = this.gameObject.tag.ToString();
         playerControlled = false;
+        enemiesInRange = 0;
+        currentEnemyTransform = null;
+        attacked = false;
+        detectedEnemies = new List<Transform>();
+
         goals = GameObject.FindGameObjectsWithTag("Goal");
         RoamToNext();
-
     }
 
     void Update()
     {
         if (!playerControlled)
         {
-            Roam();
+            if (enemiesInRange == 0)
+            {
+                Roam();
+            }
+            else
+            {
+                Transform enemyTransform = detectedEnemies[0];
+                FollowEnemy(enemyTransform);
+            }
         }
     }
 
@@ -68,10 +87,100 @@ public class Droid : MonoBehaviour
         agent.stoppingDistance = 0f;
     }
 
-    // public void Attack(){
-    //     Debug.Log("Attacked");
-    //     Instantiate(ball, new Vector3(transform.position.x,transform.position.y+1,
-    //         transform.position.z), Quaternion.identity, this.transform);
-    // }
+    Vector3 getMovingDirection()
+    {
+        Vector3 dir = GetComponent<Rigidbody>().velocity.normalized;
+        if (dir.magnitude == 0)
+        {
+            dir = Vector3.forward;
+        }
+        return dir;
+
+    }
+
+    public Vector3 getAttackDirection()
+    {
+        return movingDirection;
+    }
+
+    public string getTeam()
+    {
+        return Team;
+    }
+
+    public void EnemyEnterDetection(Collider other)
+    {
+        if (!playerControlled)
+        {
+            if (other.name == "Droid" && other.tag != Team)
+            {
+                if (enemiesInRange == 0)
+                {
+                    currentEnemyTransform = other.gameObject.transform;
+                }
+                enemiesInRange += 1;
+                detectedEnemies.Add(other.gameObject.transform);
+            }
+        }
+
+    }
+
+    public void EnemyExitDetection(Collider other)
+    {
+        if (!playerControlled)
+        {
+            if (other.name == "Droid" && other.tag != Team)
+            {
+                enemiesInRange -= 1;
+                detectedEnemies.Remove(other.gameObject.transform);
+                if (enemiesInRange > 0)
+                {
+                    currentEnemyTransform = detectedEnemies[0];
+                }
+                else
+                {
+                    RoamToNext();
+                }
+            }
+        }
+    }
+
+    void FollowEnemy(Transform enemy)
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (tag == "Orange")
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.blue;
+        }
+        Gizmos.DrawWireSphere(transform.position, 1.5f);
+    }
+
+    public void Attack(Vector3 unitVector)
+    {
+        if (!attacked)
+        {
+            movingDirection = unitVector;
+            Debug.Log(GetComponent<Rigidbody>().velocity);
+            Vector3 position = new Vector3(transform.position.x, 0.25f, transform.position.z);
+            Instantiate(ball, position, Quaternion.identity, this.transform);
+            attacked = true;
+            StartCoroutine(AttackReset());
+        }
+    }
+
+    IEnumerator AttackReset()
+    {
+        yield return new WaitForSeconds(2f);
+        attacked = false;
+    }
 
 }
+
