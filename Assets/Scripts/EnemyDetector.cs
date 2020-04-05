3ef1c0d4d48a1eb6bebd957cc04a0dcb60ab5fb4
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-05 17:42:24 +0530

    added collider to detect enemy

---
+++ Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    Droid droid;
+
+    private void Start() {
+        droid = GetComponentInParent<Droid>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        droid.EnemyEnterDetection(other);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        droid.EnemyExitDetection(other);
+    }
+}
