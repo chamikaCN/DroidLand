69b2c277f8e495c6a2f27375869ca2d74c345587
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-10 19:19:01 +0530

    animator scripts added

---
+++ Assets/Scripts/DroidAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroidAnimator : MonoBehaviour
+{
+    Animator playerAnim;
+    NavMeshAgent agent;
+    void Start()
+    {
+        playerAnim = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float speed = agent.velocity.magnitude / agent.speed;
+        playerAnim.SetFloat("Move", speed);
+    }
+
+    public void ShootAnim(){
+        playerAnim.SetTrigger("Shoot");
+    }
+}
