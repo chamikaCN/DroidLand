3a70dfaaee026b47255bc51db822efb270103fdf
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-13 12:25:59 +0530

    bomb script created

---
+++ Assets/Scripts/bomb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(DeleteExplotion());
+    }
+
+    IEnumerator DeleteExplotion()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(this.gameObject);
+    }
+}
