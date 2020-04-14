fe8484b4260e36f5df1a2e8968047800b010ac3a
chamikaCN <chamikanandasiri97@gmail.com>
2020-04-14 18:07:43 +0530

    time based rotaion added

--- Assets/Scripts/ProtectionGuard.cs
+++ Assets/Scripts/ProtectionGuard.cs
@@ -6,6 +6,6 @@ public class ProtectionGuard : MonoBehaviour
 {
     void Update()
     {
-        transform.Rotate(new Vector3(10f, 0, 0), Space.World);
+        transform.Rotate(new Vector3(10f, 0, 0)*Time.deltaTime*50f, Space.World);
     }
 }
